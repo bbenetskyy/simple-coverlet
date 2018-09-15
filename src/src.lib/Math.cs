7f7831437b6d0e2fa7e824cd7ac0a32cbf340843
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2018-09-15 20:33:17 +0200

    Init Lib with default Math static class

---
+++ src/src.lib/Math.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace src.lib
+{
+    public static class Math
+    {
+        public static int Add(int a, int b) => a + b;
+        public static int Sub(int a, int b) => b == 0 ? 0 : a - b;
+        public static int Mul(int a, int b) => a * b;
+        public static int Div(int a, int b) => a / b;
+    }
+}
